88d847bb7f57cdcaa613fd25c300f21855392b68
Slim Abida <slimabida21@gmail.com>
2026-03-10 17:55:15 +0100

    Feat: implémentation du Footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { useEffect } from "react"; 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./components/Home"; 
 import Login from "./components/Login";
 import { auth } from "./firebase";
@@ -35,7 +36,7 @@ function App() {
           path="/"
           element={
             <>
-              <Header /> <Home />
+              <Header /> <Home /> <Footer />
             </>
           }
         />
---
+++ src/components/Footer.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import Logo from "../images/header-logo.png";
+import shoppingCart from "../images/icons/shopping-cart.png";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Link to="/" className="footer-logoLink">
+        <img src={Logo} className="footer-logo" alt="logo" />
+      </Link>
+      <div className="footer-nav">
+        <Link to="/login">
+          <div className="footer-option">
+            <span className="footer-optionLineOne">Hello Guest</span>
+            <span className="footer-optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+        <Link to="/orders">
+          <div className="footer-option">
+            <span className="footer-optionLineOne">Returns</span>
+            <span className="footer-optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+        <div className="footer-option">
+          <span className="footer-optionLineOne">Your</span>
+          <span className="footer-optionLineTwo">Prime</span>
+        </div>
+        <Link to="/checkout">
+          <div className="footer-optionBasket">
+            <img
+              src={shoppingCart}
+              alt="shoppingCart"
+              width="20px"
+              height="20px"
+            />
+            <span className="footer-optionLineTwo footer-basketCount">Cart</span>
+          </div>
+        </Link>
+      </div>
+      <div className="footer-bottom">
+        <span className="footer-copyright">
+          © {new Date().getFullYear()} Amazon Clone. Tous droits réservés.
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
